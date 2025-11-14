efdbdac8745b51b67aad944cced1214c133dcc67
k87938433-sudo <k87938433@gmail.com>
2025-11-15 00:03:47 +0200

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,17 +1,21 @@
 <?php include 'header.php'; ?>
 
-<h1>Карта сайта</h1>
-<ul class="sitemap">
-    <li><a href="index.php">Главная</a></li>
-    <li><a href="about.php">О банке</a></li>
-    <li><a href="products.php">Продукты</a></li>
-    <li><a href="internet.php">Интернет-банк</a></li>
-    <li><a href="news.php">Новости</a></li>
-    <li><a href="contacts.php">Контакты</a></li>
-    <li><a href="credit_application.php">Заявка на кредит</a></li>
-    <li><a href="deposit_application.php">Заявка на вклад</a></li>
-    <li><a href="login.php">Вход</a></li>
-    <li><a href="register.php">Регистрация</a></li>
-</ul>
+<section class="page-section sitemap-page">
+    <h1>Карта сайта</h1>
+
+    <ul class="sitemap-list">
+        <li><a href="/index.php">Главная</a></li>
+        <li><a href="/about.php">О банке</a></li>
+        <li><a href="/products.php">Продукты</a></li>
+        <li><a href="/internet.php">Интернет-банк</a></li>
+        <li><a href="/news.php">Новости</a></li>
+        <li><a href="/contacts.php">Контакты</a></li>
+        <li><a href="/credit_application.php">Заявка на кредит</a></li>
+        <li><a href="/deposit_application.php">Заявка на вклад</a></li>
+        <li><a href="/login.php">Вход</a></li>
+        <li><a href="/register.php">Регистрация</a></li>
+        <li><a href="/sitemap.php">Карта сайта</a></li>
+    </ul>
+</section>
 
 <?php include 'footer.php'; ?>
