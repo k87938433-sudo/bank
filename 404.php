e85b37f784a1842ba8a12fb4a0a7281ca21efc60
k87938433-sudo <k87938433@gmail.com>
2025-11-15 00:04:06 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,11 @@
 <?php include 'header.php'; ?>
 
-<h1>Ошибка 404</h1>
-<p>Запрашиваемая страница не найдена.</p>
-<p><a href="index.php">Перейти на главную страницу</a></p>
+<section class="page-section error-page">
+    <h1>404 — страница не найдена</h1>
+
+    <p>К сожалению, запрашиваемая страница недоступна или была удалена.</p>
+
+    <a href="/index.php" class="btn-primary">Вернуться на главную</a>
+</section>
 
 <?php include 'footer.php'; ?>
