f1a5fe3b22a33e54f8d6736b34573739adafcc76
k87938433-sudo <k87938433@gmail.com>
2025-11-14 20:47:18 +0200

    Delete 404.php

--- 404.php
+++
@@ -1,7 +0,0 @@
-<?php include 'header.php'; ?>
-
-<h1>Ошибка 404</h1>
-<p>Запрашиваемая страница не найдена.</p>
-<p><a href="index.php">Перейти на главную страницу</a></p>
-
-<?php include 'footer.php'; ?>
